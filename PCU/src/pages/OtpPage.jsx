d47b4357fda4d3085f88dbf62ff5e5287c249194
Arlen20 <161277748+Arlen20@users.noreply.github.com>
2025-06-19 09:37:50 +0500

    Add files via upload

---
+++ PCU/src/pages/OtpPage.jsx
@@ -0,0 +1,15 @@
+import NavbarPage from '../Components/NavbarPage'
+import Otp2 from '../Components/OtpVerify'
+
+function OtpPage() {
+	return (
+		<div>
+			<NavbarPage />
+			<div className='container mt-5'>
+				<Otp2 />
+			</div>
+		</div>
+	)
+}
+
+export default OtpPage
