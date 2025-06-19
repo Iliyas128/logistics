117062c7896f2468591bf3452c57ba1042e2ed5d
Arlen20 <161277748+Arlen20@users.noreply.github.com>
2025-06-19 09:39:27 +0500

    Update App.jsx

--- PCU/src/App.jsx
+++ PCU/src/App.jsx
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
+import OtpVerify from './pages/OtpPage'
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path='/verify-otp' element={<OtpVerify />} />
       </Routes>
     </>
   )
